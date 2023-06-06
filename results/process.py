ccc4dd9fecd75c68fabbbdba37d7c2ddf162bcc2
Hanchen Li <lihanc2002@uchicago.edu>
2023-06-06 14:43:11 -0500

    videos

--- results/process.py
+++ results/process.py
@@ -3,19 +3,22 @@ import numpy as np
 import os, sys
 
 def isvalid(df):
-    cond1 = (df["usertimes"] > df["systemtimes"]).all()
+    cond1 = df.query("videos == '5.mp4'")["sanityscore"].item() != 2
+    cond6 = df.query("videos == '5.mp4'")["scores"].item() <= df.query("videos == '4.mp4'")["scores"].item()
     cond2 = df.query("videos == '1.mp4'")["scores"].item() == df["scores"].max()
     cond3 = df.query("videos == '2.mp4'")["scores"].item() == df["scores"].min()
     cond4 = df.query("videos == '1.mp4'")["sanityscore"].item() == 2
     cond5 = df.query("videos == '2.mp4'")["sanityscore"].item() == 5
     #cond6 = (df.query("videos == '3.mp4'")["sanityscore"].item() == 5) or (df.query("videos == '3.mp4'")["sanityscore"].item() == 4) 
-    return cond1 and cond2 and cond3 and cond4 and cond5 #and cond6
+    return cond1 and cond2 and cond3 and cond4 and cond5 and cond6
     #return True
 
 def process_one_file(filename):
     with open(filename, "r") as fin:
         lines = [l.strip("\n") for l in fin]
     scores = [int(v) for v in lines[0].split(',')]
+    if (len(scores) != 6):
+        return None
     #videos = [f"{v}.mp4" for v in lines[1].split(",")]
     videos = [f"{v}.mp4" for v in range(1, 10)]
     videos = videos[:len(scores)]
